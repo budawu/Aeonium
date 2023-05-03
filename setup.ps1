04e2733e7f52f46758a55b71fd04d2b13a98190b
budawu <1183972971@qq.com>
2023-05-03 16:10:30 +0800

    remake shell script

--- setup.ps1
+++ setup.ps1
@@ -1,12 +1,10 @@
-function Set-AeoniumUp($venv_mane,$highlight_theme) {
-    python -m venv $venv_mane
-    .\$venv_mane\Scripts\activate
-    pip install -r requirements.txt #下载依赖的包
+python -m venv ll_env
+.\ll_env\Scripts\activate
+pip install -r requirements.txt #下载依赖的包
 
-    pygmentize -S $highlight_theme -f html -a .codehilite > themes/highlight/highlight.css #语法高亮的css
+pygmentize -S monokai -f html -a .codehilite > themes/highlight/highlight.css #语法高亮的css
 
-    python manage.py makemigrations aeonium
-    python manage.py migrate
-    python manage.py createsuperuser
-
-}
+python cli/secret_key.py
+python manage.py makemigrations aeonium
+python manage.py migrate
+python manage.py createsuperuser
--- setup.sh
+++ setup.sh
@@ -1,10 +1,11 @@
-#!usr/bin/env sh
-python -m venv $1
-$1/bin/activate
+#!usr/bin/env bash
+python -m venv ll_env
+source ll_env/bin/activate
 pip install -r requirements.txt #下载依赖的包
 
-pygmentize -S $2 -f html -a .codehilite > themes/highlight/highlight.css #语法高亮的css
+pygmentize -S monokai -f html -a .codehilite > themes/highlight/highlight.css #语法高亮的css
 
+python cli/secret_key.py
 python manage.py makemigrations aeonium
 python manage.py migrate
 python manage.py createsuperuser
