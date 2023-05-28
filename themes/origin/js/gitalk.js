efb69931d11cdb9343fcb73be49341a1f7e8f29c
budawu <1183972971@qq.com>
2023-05-28 22:10:35 +0800

    改了语法高亮颜色（部分)

--- themes/origin/js/gitalk.js
+++ themes/origin/js/gitalk.js
@@ -1,14 +1,9 @@
-import { createRequire } from 'module';
-const require = createRequire(import.meta.url);
-const fs = require('fs');
-const fileContents = fs.readFileSync('../config.origin.yml', 'utf8');
-const data = yaml.safeLoad(fileContents);
 var gitalk = new Gitalk({
-    clientID: data['id'],
-    clientSecret: data['clientSecret'],
-    repo: data['repo'],
-    owner: data['owner'],
-    admin:  ['admin'],
+    clientID: 'id',
+    clientSecret: 'clientSecret',
+    repo: 'repo',
+    owner: 'owner',
+    admin: 'admin',
     id: location.pathname,      // Ensure uniqueness and len
     distractionFreeMode: false  // Facebook-like distraction
 })
